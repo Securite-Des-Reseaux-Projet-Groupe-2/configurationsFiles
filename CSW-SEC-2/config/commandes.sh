873a70464ca4c2eb5324fd23e683e95348c49f1c
alexandrebrugger <superalexbrugger@gmail.com>
2024-11-28 14:06:59 +0100

    SDRPT-40 Configurations Vlan Secrétariat

---
+++ CSW-SEC-2/config/commandes.sh
@@ -0,0 +1,37 @@
+# Conf t
+conf t
+
+# Configuration Vlan
+vlan 10
+name SEC
+exit
+
+# Configuration de l'interface Vlan
+interface Vlan 10
+ip address 10.1.1.3 255.255.255.0
+no shutdown
+exit
+
+# Configuration HSRP
+interface Vlan 10
+standby version 2
+standby 10 ip 10.1.1.1
+standby 10 priority 100
+standby 10 preempt
+exit
+
+# Configuration EtherChannel
+interface range fastEthernet 1/0 - 1
+switchport trunk encaplulation dotq1
+switchport mode trunk
+channel-group 1 mode on
+
+interface Port-channel1
+switchport trunk encaplulation dotq1
+switchport mode trunk
+switchport trunk allowed vlan 10
+exit
+
+# Enregistrement des config dans la cache à long terme
+exit
+copy run start
---
+++ SL2-SEC-1/config/commandes.sh
@@ -0,0 +1,24 @@
+# Conf t
+conf t
+
+# Configuration Vlan
+vlan 10
+name SEC
+exit
+
+# Configuration des ports ACCESS
+interface range fastEthernet 1/4 - 5
+switchport mode access
+switchport access vlan 10
+exit
+
+# Configuration des ports TRUNK
+
+interface range fastEthernet 1/2 - 3
+switchport mode trunk
+switchport trunk allowed vlan 10
+exit
+
+# Enregistrement des config dans la cache à long terme
+exit
+copy run start
