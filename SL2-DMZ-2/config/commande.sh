b5ba07e4441acaea43f6c74172bceaf028fcc6e9
Ichiky_Otsu <bricedelcroix20@gmail.com>
2024-11-30 11:29:28 +0100

    Workflow

---
+++ SL2-DMZ-2/config/commande.sh
@@ -0,0 +1,34 @@
+
+configure terminal
+
+no ip routing
+
+vlan 100
+name DMZ
+no shut
+exit
+
+interface range fastEthernet 1/4
+switchport mode access
+switchport access vlan 100
+no shut
+exit
+
+interface range fastEthernet 1/2 - 3
+switchport mode trunk
+switchport trunk allowed vlan 1-2,100,1002-1005
+no shut
+exit
+
+int vlan100
+ip address 10.100.0.242 255.255.255.0
+no shut
+exit
+
+ip default-gateway 10.100.0.1
+
+spanning-tree vlan 100 priority 32768
+
+end
+copy running-config startup-config
+
