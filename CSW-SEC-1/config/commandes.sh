921cc8f240a15f5938b70dde550c78727e71a586
maxcorte <147084320+maxcorte@users.noreply.github.com>
2024-12-01 12:52:07 +0100

    interface + ospf

--- CSW-SEC-1/config/commandes.sh
+++ CSW-SEC-1/config/commandes.sh
@@ -12,10 +12,38 @@ ip address 10.1.1.2 255.255.255.0
 no shutdown
 exit
 
+#configuration fastEthernet 1/2
+
 interface fastEthernet 1/2
 switchport trunk encapsulation dot1q
 switchport mode trunk
 exit
+
+#configuration fastEthernet 0/0
+
+interface fastEthernet 0/0
+ip address 10.1.0.2 255.255.255.252
+no shutdown
+exit
+
+#configuration fastEthernet 0/1
+
+interface fastEthernet 0/1
+ip address 10.1.0.6 255.255.255.252
+no shutdown
+exit
+
+
+#configuration ospf
+
+ip routing
+router ospf 1
+network 10.1.0.0 0.0.0.3 area 0
+network 10.1.0.4 0.0.0.3 area 0
+network 10.1.1.0 0.0.0.255 area 0
+passive-interface fastEthernet 1/2
+exit
+
 
 # Configuration HSRP
 interface Vlan 10
