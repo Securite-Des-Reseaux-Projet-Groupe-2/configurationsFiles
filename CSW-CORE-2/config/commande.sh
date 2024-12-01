527a305584b5b121261aaaf515460853e6eb6e80
maxcorte <147084320+maxcorte@users.noreply.github.com>
2024-12-01 13:46:16 +0100

    Update commande.sh

--- CSW-CORE-2/config/commande.sh
+++ CSW-CORE-2/config/commande.sh
@@ -1,1 +1,46 @@
+conf t
 
+#configuration fastEthernet 0/0
+
+interface fastEthernet 0/0
+ip address 10.1.0.13 255.255.255.252
+no shutdown
+exit
+
+#configuration fastEthernet 0/1
+
+interface fastEthernet 0/1
+ip address 10.1.0.9 255.255.255.252
+no shutdown
+exit
+
+#configuration fastEthernet 1/2
+
+interface fastEthernet 1/2
+no switchport
+ip address 10.1.0.21 255.255.255.252
+no shutdown
+exit
+
+#configuration fastEthernet 1/3
+
+interface fastEthernet 1/3
+no switchport
+ip address 10.1.0.29 255.255.255.252
+no shutdown
+exit
+
+
+#configuration ospf
+
+ip routing
+router ospf 1
+network 10.1.0.8 0.0.0.3 area 0
+network 10.1.0.12 0.0.0.3 area 0
+network 10.1.0.20 0.0.0.3 area 0
+network 10.1.0.28 0.0.0.3 area 0
+exit
+
+end
+
+copy run start
