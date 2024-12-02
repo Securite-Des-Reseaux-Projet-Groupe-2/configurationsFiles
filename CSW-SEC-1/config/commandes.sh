5fa851f9620f6ea87691e488f6a87b9c7f67b60f
Ichiky_Otsu <bricedelcroix20@gmail.com>
2024-12-02 19:17:26 +0100

    Rassemblement des configurations

--- CSW-SEC-1/config/commandes.sh
+++ CSW-SEC-1/config/commandes.sh
@@ -9,7 +9,13 @@ exit
 # Configuration de l'interface Vlan
 interface Vlan 10
 ip address 10.1.1.2 255.255.255.0
-no shutdown
+# Configuration HSRP
+standby version 2
+standby 10 ip 10.1.1.1
+standby 10 priority 100
+standby 10 preempt
+standby 10 name SERV
+no shut
 exit
 
 interface fastEthernet 1/2
@@ -17,13 +23,6 @@ switchport trunk encapsulation dot1q
 switchport mode trunk
 exit
 
-# Configuration HSRP
-interface Vlan 10
-standby version 2
-standby 10 ip 10.1.1.1
-standby 10 priority 100
-standby 10 preempt
-exit
 
 # Configuration EtherChannel
 interface range fastEthernet 1/0 - 1
@@ -34,7 +33,7 @@ channel-group 1 mode on
 interface Port-channel1
 switchport trunk encapsulation dot1q
 switchport mode trunk
-# switchport trunk allowed vlan 10
+switchport trunk allowed vlan 1-2,10,20,30,100,1002-1005
 exit
 
 # Enregistrement des config dans la cache à long terme
