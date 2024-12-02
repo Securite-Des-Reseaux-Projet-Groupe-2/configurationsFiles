85129aa62bc359104b5b8c751f1a064c2c5fb15d
Ichiky_Otsu <bricedelcroix20@gmail.com>
2024-12-02 19:57:12 +0100

    Vlan medecin correction

--- CSW-MED-2/config/commandes.sh
+++ CSW-MED-2/config/commandes.sh
@@ -1,6 +1,6 @@
 # Conf t
 conf t
-
+ip routing
 # Configuration VLAN
 vlan 20
 name MED
@@ -9,20 +9,20 @@ exit
 # Configuration de l'interface VLAN
 interface Vlan 20
 ip address 10.1.2.3 255.255.255.0
-no shutdown
+# Configuration HSRP
+standby version 2
+standby 20 ip 10.1.2.1
+standby 20 priority 100
+standby 20 preempt
+standby 20 name MED
+no shut
+exit 
 
 interface fastEthernet 1/3
 switchport trunk encapsulation dot1q
 switchport mode trunk
 exit
 
-# Configuration HSRP
-interface Vlan 20
-standby version 2
-standby 10 ip 10.1.2.1
-standby 10 priority 110
-standby 10 preempt
-exit
 
 # Configuration EtherChannel
 interface range fastEthernet 1/0 - 1
@@ -33,7 +33,7 @@ channel-group 1 mode on
 interface Port-channel1
 switchport trunk encapsulation dot1q
 switchport mode trunk
-# switchport trunk allowed vlan 20
+switchport trunk allowed vlan 1-2,10,20,30,100,1002-1005
 exit
 
 # Enregistrer les config dans la cache à long terme (sauf les vlans pour l'instant)
--- SL2-MED-1/config/commandes.sh
+++ SL2-MED-1/config/commandes.sh
@@ -1,30 +1,37 @@
 # Conf t
 conf t
 no ip routing
-# Configuration VLAN
+# Configuration Vlan
 vlan 20
 name MED
 exit
 
 # Configuration des ports ACCESS
-
-interface range fastEthernet 1/4 - 5
+interface fastEthernet 1/4 
 switchport mode access
 switchport access vlan 20
-
-# Configuration des pors TRUNK
-
-interface range fastEthernet 1/2 - 3
-switchport mode trunk
-# switchport trunk allowed vlan 20
 exit
 
-# Configuration default-gateway
+interface range fastEthernet 1/5
+switchport mode access
+switchport access vlan 20
+exit
 
+# Configuration des ports TRUNK
+
+interface fastEthernet 1/2
+switchport mode trunk
+switchport trunk allowed vlan 1-2,10,20,30,100,1002-1005
+exit
+
+interface fastEthernet 1/3
+switchport mode trunk
+switchport trunk allowed vlan 1-2,10,20,30,100,1002-1005
+exit
+
+interface vlan 20
+ip address 10.1.2.241 255.255.255.0
 ip default-gateway 10.1.2.1
 
-# Enregistrer les config dans la cache à long terme (sauf les vlans pour l'instant)
-exit
+end
 copy run start
-
-
