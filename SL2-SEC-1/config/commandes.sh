8e64ec2f93c58535cee96c833971bd4f81716f42
Y-Rowz <146989128+Y-Rowz@users.noreply.github.com>
2024-12-03 21:41:23 +0100

    no shut + note STP

--- SL2-SEC-1/config/commandes.sh
+++ SL2-SEC-1/config/commandes.sh
@@ -10,11 +10,13 @@ exit
 interface fastEthernet 1/4 
 switchport mode access
 switchport access vlan 10
+no shut
 exit
 
 interface range fastEthernet 1/5
 switchport mode access
 switchport access vlan 10
+no shut
 exit
 
 # Configuration des ports TRUNK
@@ -22,16 +24,23 @@ exit
 interface fastEthernet 1/2
 switchport mode trunk
 switchport trunk allowed vlan 1-2,10,20,30,100,1002-1005
+no shut
 exit
 
 interface fastEthernet 1/3
 switchport mode trunk
 switchport trunk allowed vlan 1-2,10,20,30,100,1002-1005
+no shut
 exit
 
 interface vlan 10
 ip address 10.1.1.241 255.255.255.0
+no shut
+exit
+
 ip default-gateway 10.1.1.254
+
+#ajouter spanning-tree
 
 end
 copy run start
