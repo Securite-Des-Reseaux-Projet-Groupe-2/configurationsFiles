00ac67542bc6fbaa11f54edc633ed1398a84e294
Ichiky_Otsu <bricedelcroix20@gmail.com>
2024-12-03 08:59:07 +0100

    lol

--- CSW-CORE-2/config/commandes.sh
+++ CSW-CORE-2/config/commandes.sh
@@ -9,29 +9,7 @@ vlan 20
 name MED
 exit
 
-# Configuration de l'interface VLAN
-interface Vlan 20
-ip address 10.1.2.253 255.255.255.0
-# Configuration HSRP
-standby version 2
-standby 20 ip 10.1.2.254
-standby 20 priority 100
-standby 20 preempt
-standby 20 name MED
-no shut
-exit 
 
-# Configuration de l'interface VLAN
-interface Vlan 10
-ip address 10.1.1.253 255.255.255.0
-# Configuration HSRP
-standby version 2
-standby 10 ip 10.1.1.254
-standby 10 priority 100
-standby 10 preempt
-standby 10 name SEC
-no shut
-exit 
 
 # Configuration de l'EtherChannel avec CSW-CORE-1
 interface range FastEthernet1/0 - 1
